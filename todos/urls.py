aa4960f3b5b7979d59da17ab440178f4be1c984b
osm3rr <osm3rr@adakademy.com>
2025-01-29 16:56:26 -0400

    todo detail done

--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListTodo
+from .views import ListTodo, DetailTodo
 
 urlpatterns = [
     path('', ListTodo.as_view(), name='todos_list'),
+    path('<int:pk>/', DetailTodo.as_view(), name='todos_detail'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -8,3 +8,7 @@ from .serializers import TodoSerializer
 class ListTodo(generics.ListAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
